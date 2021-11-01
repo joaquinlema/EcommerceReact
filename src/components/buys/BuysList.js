418b6f7e017a59bfda052effdd4dc4669cd0cf83
Joaquin Lema <joaquin.lema.sp@SofttekProjects.com>
2021-11-01 11:12:55 -0300

    ajustamos cargando a nivel pagina

--- src/components/buys/BuysList.js
+++ src/components/buys/BuysList.js
@@ -3,9 +3,11 @@ import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import BuysItem from './BuysItem';
 import { useSelector } from 'react-redux';
+import { Alert } from '@mui/material';
 
 export default function BuysList() {
     const { productosComprados } = useSelector(state => state.ProductReducer);
+    if (productosComprados.length === 0) return <Alert severity="info">Sin compras Realizadas !!!!</Alert>
 
     return (
         <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material';
+import { Alert, Grid } from '@mui/material';
 import React, { Fragment } from 'react'
 import { useSelector } from 'react-redux';
 import ProductItem from './ProductItem';
@@ -8,20 +8,20 @@ const ProductList = () => {
 
     const { searchResults, loading } = useSelector(state => state.SearchReducer);
 
+    if (loading) return <ProductLoading />
+
+    if (searchResults.length === 0) return <Alert severity="info">Por favor realice una busqueda!!!!</Alert>
+
     return (
         <Fragment>
-            {
-                loading
-                    ? <ProductLoading />
-                    :
-                    <Grid container spacing={1} alignItems="stretch">
-                        {searchResults.map((elem, index) => (
-                            <Grid item xs={4} key={index}>
-                                <ProductItem data={elem} />
-                            </Grid>
-                        ))}
+
+            <Grid container spacing={1} alignItems="stretch">
+                {searchResults.map((elem, index) => (
+                    <Grid item xs={4} key={index}>
+                        <ProductItem data={elem} />
                     </Grid>
-            }
+                ))}
+            </Grid>
         </Fragment>
     )
 }
